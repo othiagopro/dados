2aaa33b71f50cc5e6c1ad4f3425197ef56c0ab01
othiagopro <thiagomendo.ti@gmail.com>
2025-12-09 07:53:05 -0400

    New repositorio

---
+++ dados/ex01.py
@@ -0,0 +1,27 @@
+                        # Listas
+
+filmes = ['O rei arthur', 'It a coisa', 'Truque de mestre']
+filmes.append('Zootopia 2')
+filmes.insert(0, 'O predador')
+
+#filmes[0] - Verifica o que tem na posição 0 dentro da lista
+# print(filmes)                     
+                        
+                        # Tuplas
+
+# Imutavel
+data = ('12/01', '25/02', '26/03')
+
+v = filmes, data
+
+print(v)
+
+                        # Dicionario
+
+# Não ordenada, Mútavel, Chave: valor
+
+
+
+
+
+
