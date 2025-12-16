645df3a9fa526740b67b5f53511f08842f45a7a1
othiagopro <thiagomendo.ti@gmail.com>
2025-12-16 07:58:22 -0400

    Função open (w,r)

--- dados/ex01.py
+++ dados/ex01.py
@@ -1,16 +1,16 @@
                         # Listas
 
 filmes = ['O rei arthur', 'It a coisa', 'Truque de mestre']
-filmes.append('Zootopia 2')
-filmes.insert(0, 'O predador')
+# filmes.append('Zootopia 2')
+# filmes.insert(0, 'O predador')
 
 #filmes[0] - Verifica o que tem na posição 0 dentro da lista
-# print(filmes)                     
-                        
+# print(filmes)
+
                         # Tuplas
 
 # Imutavel
-data = ('12/01', '25/02', '26/03')
+# data = ('12/01', '25/02', '26/03')
 
 
                         # Dicionario
@@ -21,7 +21,14 @@ data = ('12/01', '25/02', '26/03')
 # dados['Pedro'] = 12
 # print(dados)
 
-filmes_dict = {}
+filmes_dict = {'O rei arthur': '12/01', 'It a coisa': '25/02', 'Truque de mestre': '26/03'}
+# print(filmes_dict)
+
+for k, i in filmes_dict.items():
+    print(f'{k} : {i}')
+
+
+
 
 
 
