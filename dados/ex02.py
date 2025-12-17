285d06de08f6bc31065a4ee51a8143f238cd05da
othiagopro <thiagomendo.ti@gmail.com>
2025-12-17 07:52:24 -0400

    Finalizado

--- dados/ex02.py
+++ dados/ex02.py
@@ -7,29 +7,38 @@
 # CSV - Serve para grande quantidades de dados
 # JSON - Converter JSON para dict e vice versa
 
-##                  TXT
-# with open('./dados/dados.txt', 'w') as t:
-#      t.write('Teste, estou testando essa função, legal funcionou, teste')
+#                  TXT
+with open('./dados/dados.txt', 'w') as t:
+    t.write('Teste, estou testando essa função, legal funcionou, teste')
 
-# with open('./dados/dados.txt', 'a') as t:
-#     t.write('Ultima linha')
+with open('./dados/dados.txt', 'a') as t:
+    t.write('Ultima linha')
 
-# with open('./dados/dados.txt', 'r') as t:
-#     conteudo = t.read()
-# print(conteudo)
+with open('./dados/dados.txt', 'r') as t:
+    conteudo = t.read()
+    print(conteudo)
 
-##                 CSV
-# import csv
+#                 CSV
+import csv
 
-# with open('./dados/dados.csv', 'w') as t:
-#      escritor = csv.writer(t)
-#      escritor.writerow(['nome', 'idade'])
-#      escritor.writerow(['Thiago', '22'])
-#      escritor.writerow(['Janderson' '55'])
+with open('./dados/dados.csv', 'w') as t:
+    escritor = csv.writer(t)
+    escritor.writerow(['nome', 'idade'])
+    escritor.writerow(['Thiago', '22'])
+    escritor.writerow(['Janderson' '55'])
 
-# with open('./dados/dados.csv', newline='') as t:
-#     leitor = csv.reader(t)
-#     for linha in leitor:
-#         print(linha)
+with open('./dados/dados.csv', newline='') as t:
+    leitor = csv.reader(t)
+    for linha in leitor:
+        print(linha)
 
-##                JSON
+#                JSON
+import json
+
+dados = {'nome': 'Thiago', 'idade': '22', 'enderecos':['a','b']}
+with open('./dados/dados.json', 'w') as t:
+    json.dump(dados, t)
+
+with open('./dados/dados.json', 'r') as t:
+    dados_lidos = json.load(t)
+    print(dados_lidos)
